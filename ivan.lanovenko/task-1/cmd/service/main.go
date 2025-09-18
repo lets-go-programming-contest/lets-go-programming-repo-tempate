6708c40f4dfdbeff9cbffd1af55442d00cc0eb92
Tuc0Sa1amanka <sparowj765@gmail.com>
2025-09-18 21:38:32 +0300

    [TASK-1] Implementation of calculator and main added

--- ivan.lanovenko/task-1/cmd/service/main.go
+++ ivan.lanovenko/task-1/cmd/service/main.go
@@ -1,4 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"github.com/Tuc0Sa1amanka/task-1/pkg/calculator"
+)
+
 func main() {
+	var a int = 0
+	var b int = 0
+	var c rune = '0'
+
+	if _, err := fmt.Scanln(&a); err != nil {
+		fmt.Println("Invalid first operand")
+		os.Exit(1)
+	}
+
+	if _, err := fmt.Scanln(&b); err != nil {
+		fmt.Println("Invalid second operand")
+		os.Exit(1)
+	}
+
+	_, err := fmt.Scanf("%c", &c)
+	if err != nil || !calculator.IsOperator(c) {
+		fmt.Println("Invalid operation")
+		os.Exit(1)
+	}
+
+	switch c {
+	case '+':
+		fmt.Print(calculator.Sum(a, b))
+	case '-':
+		fmt.Print(calculator.Dif(a, b))
+	case '*':
+		fmt.Print(calculator.Mul(a, b))
+	case '/':
+		res, err := calculator.Div(a, b)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+		fmt.Print(res)
+	}
 }
